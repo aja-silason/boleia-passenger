e5449a70b35aa3aad32a53407799742121a59f72
aja-silason <ananiasjaimeuagusto@gmail.com>
2026-01-25 15:19:47 +0100

    build: new build

--- app/view/WelcomeScreen.tsx
+++ app/view/WelcomeScreen.tsx
@@ -11,7 +11,7 @@ export default function WelcomeScreen() {
   return (
     <View style={style.mainContainer}>
       <ImageBackground 
-        source={require("@/assets/app_images/passanger.png")} 
+        source={require("@/assets/app_images/passanger.jpg")} 
         style={style.background}
         resizeMode="cover"
       >
