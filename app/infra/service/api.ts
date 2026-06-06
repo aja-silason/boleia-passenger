c374710079a3794aa11942817b1599a4bc2091cd
aja-silason <ananiasjaimeuagusto@gmail.com>
2026-06-06 23:24:45 +0100

    refactor: add driver module into to unified to one single application

--- app/infra/service/api.ts
+++ app/infra/service/api.ts
@@ -3,6 +3,5 @@ import { API_CONFIG } from "./config";
 
 export const api = axios.create({
     baseURL: API_CONFIG.BASE_URL,
-    // baseURL: process.env.EXPO_PUBLIC_API_URL,
     withCredentials: true
 });
